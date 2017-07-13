b1903bfda63bbee1a86fedb822e7de5953ed552a
cefjoeii <cefjoeii@gmail.com>
2017-07-13 15:16:20 +0800

    Add error handling middleware

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const config = require('./config/db');
 
@@ -9,12 +9,13 @@ mongoose.Promise = global.Promise;
 
 // Connect to the database
 mongoose.connect(config.db);
+let db = mongoose.connection;
 
-mongoose.connection.on('connected', () => {
+db.on('open', () => {
   console.log('Connected to the database.');
 });
 
-mongoose.connection.on('error', (err) => {
+db.on('error', (err) => {
   console.log('Database error: ' + err);
 });
 
@@ -27,8 +28,16 @@ app.use(express.static('public'));
 // Set body parser middleware
 app.use(bodyParser.json());
 
-// Initialize routes
-app.use('/api', require('./routes/users'));
+// Initialize routes middleware
+app.use('/api/users', require('./routes/users'));
+
+// Use express's default error handling middleware
+app.use(function (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+  res.status(400).json({ err: err });
+});
 
 // Start the server
 const port = process.env.PORT || 3000;
